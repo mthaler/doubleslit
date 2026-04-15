3944f9d4bc7027ec49a6a14a1c6a261aaba97909
Michael Thaler <michael.thaler2@gmail.com>
2026-04-15 09:36:01 +0200

    Updated slit.go.

--- slit.go
+++ slit.go
@@ -2,7 +2,7 @@ package main
 
 import "math"
 
-func intensity(Imax, d, th, l float64) float64 {
-
+func intensity(Imax, d, x, l float64) float64 {
+	th := math.Acos(2 * x / l)
 	return Imax * math.Cos(math.Pi*d*math.Sin(th)/l) * math.Cos(math.Pi*d*math.Sin(th)/l)
 }
