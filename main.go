3e3ca1a2467b2e071f1a52dbb96064c733410907
Michael Thaler <michael.thaler2@gmail.com>
2026-04-15 09:41:08 +0200

    Updated slit.go.

--- main.go
+++ main.go
@@ -10,4 +10,11 @@ func main() {
 	var points plotter.XYs
 
 	fmt.Println("%+v", points)
+	for i := -500; i <= 500; i++ {
+		x := float64(i) / 100.0
+		points = append(points, plotter.XY{
+			X: x,
+			Y: intensity(1, 2, x, 10),
+		})
+	}
 }
