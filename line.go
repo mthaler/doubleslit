d1a78e4b81edc01df0d906ab8db7064bc79db7e7
Michael Thaler <michael.thaler2@gmail.com>
2026-04-15 09:44:09 +0200

    Updated line.go and main.go.

--- line.go
+++ line.go
@@ -1,28 +1,27 @@
 package main
 
 import (
-	"image/color"
 	"log"
 
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
-func CreateLineplotPlot(title string, labels labels, bounds bounds, file string, lines ...*plotter.Line) {
+func CreateLineplotPlot(points plotter.XYs, title string, labels labels, bounds bounds, file string) {
 	p := plot.New()
 	p.Title.Text = title
 	p.X.Label.Text = labels.x
 	p.Y.Label.Text = labels.y
 	p.X.Min, p.X.Max, p.Y.Min, p.Y.Max = bounds.xmin, bounds.xmax, bounds.ymin, bounds.ymax
-	p.BackgroundColor = color.Transparent
-	p.Add(plotter.NewGrid())
 
-	for _, line := range lines {
-		p.Add(line)
+	err := plotutil.AddLines(p, "", points)
+	if err != nil {
+		log.Fatalf("could not create lineplot: %+v", err)
 	}
 
-	err := p.Save(20*vg.Centimeter, 10*vg.Centimeter, file)
+	err = p.Save(20*vg.Centimeter, 10*vg.Centimeter, file)
 	if err != nil {
 		log.Fatalf("could not save lineplot: %+v", err)
 	}
--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"gonum.org/v1/plot/plotter"
 )
 
 func main() {
 	var points plotter.XYs
 
-	fmt.Println("%+v", points)
 	for i := -500; i <= 500; i++ {
 		x := float64(i) / 100.0
 		points = append(points, plotter.XY{
