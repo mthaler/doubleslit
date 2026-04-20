c670e74033ed52e5d15262c75e454df65d5336f1
Michael Thaler <michael.thaler2@gmail.com>
2026-04-20 10:44:53 +0200

    Updated slit.go.

--- slit.go
+++ slit.go
@@ -3,6 +3,10 @@ package main
 import "math"
 
 func intensity(y, y0, I0, d, l float64) float64 {
-	th := math.Acos(y / y0)
-	return 4 * I0 * math.Cos(math.Pi*d*math.Sin(th)/l) * math.Cos(math.Pi*d*math.Sin(th)/l)
+	if y == 0 {
+		return 0
+	} else {
+		th := math.Acos(y / y0)
+		return 4 * I0 * math.Cos(math.Pi*d*math.Sin(th)/l) * math.Cos(math.Pi*d*math.Sin(th)/l)
+	}
 }
