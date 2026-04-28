ad28ca484210d5522bcefce817e6a88c32ff9db6
Michael Thaler <michael.thaler2@gmail.com>
2026-04-28 10:33:49 +0200

    Updated intensity.go.

--- intensity.go
+++ intensity.go
@@ -6,9 +6,7 @@ import (
 
 func intensity(y, y0, x0, d, l, b float64) float64 {
 	th := math.Atan(y / x0)
-	si := math.Sin(th)
-	//i := math.Cos(math.Pi * d * math.Sin(th) / l)
-	//diff := sinc(math.Pi * b * math.Sin(th) / l)
-	//return i * i * diff * diff
-	return math.Pi * d * si
+	i := math.Cos(math.Pi * d * math.Sin(th) / l)
+	diff := sinc(math.Pi * b * math.Sin(th) / l)
+	return i * i * diff * diff
 }
